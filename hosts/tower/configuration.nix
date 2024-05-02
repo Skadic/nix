cfc0a0059ec59000c5cfb57ba37370e87c123f44
Skadic <me@skadic.moe>
2024-05-02 18:13:55 +0200

    switch tower to grub

--- hosts/tower/configuration.nix
+++ hosts/tower/configuration.nix
@@ -27,8 +27,11 @@
   };
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  #boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
