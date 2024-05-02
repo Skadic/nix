e15fcc7804196fb61f1c27503966d6281ff309bb
Skadic <me@skadic.moe>
2024-05-02 18:02:29 +0200

    fix

--- hosts/tower/configuration.nix
+++ hosts/tower/configuration.nix
@@ -29,7 +29,6 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.useOSProber = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
